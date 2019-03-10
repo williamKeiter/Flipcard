20f9e9fafd4dfa7b503a0cfe7c70141d78c88ce3
WilliamKeiter <williamkeiter@gmail.com>
2019-03-09 15:36:52 -1000

    Updated npm dependencies, bootstrap starter, css

--- app.js
+++ app.js
@@ -1,2 +1,62 @@
 //jshint esversion:6
 require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require('passport-local-mongoose');
+const findOrCreate = require('mongoose-findorcreate');
+
+const app = express();
+
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+mongoose.connect("mongodb://localhost:27017/FlipcardDB", {useNewUrlParser: true});
+mongoose.set("useCreateIndex", true);
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  googleId: String,
+  facebookId: String,
+  Workcenter: String,
+  cafsc: String,
+  deros: String,
+  dependents: String,
+  phone: String,
+  address: String,
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000.");
+});
